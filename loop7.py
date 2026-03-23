2960e04a31a16b9b7631dd2215e94c61d0d4381d
pinky <pinky.k0201@gmail.com>
2026-03-23 17:31:41 +0530

    done

---
+++ loop7.py
@@ -0,0 +1,15 @@
+for i in range(1,51):
+    if i%2==0 and i%3==0:
+        
+        print(i,"divisible by both")
+        
+    elif i%3==0:
+        
+            print(i,"divisible by 3")
+            
+    elif i%2==0:
+        
+            print(i,"divisible by 2")
+    else:
+    
+                print("error")
